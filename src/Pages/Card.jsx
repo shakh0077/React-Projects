989eb62a3ba83b45f8bf7fa9b58c88c87af5ad9b
OybekShakh <egamberdiyivoybek@gmail.com>
2025-04-09 18:10:26 +0500

    commit data

--- src/Pages/Card.jsx
+++ src/Pages/Card.jsx
@@ -7,14 +7,13 @@ import CardProduct from '../Component/CardProduct'
 
 function Card() {
     const {cart} = useContext(CartContext)
+
     
   return (
-    <div  className='h-[30vh]'>
+    <div  className='h-[26vh]'>
     <div className='w-full max-w-[1440px]  pt-3 mx-auto relative'>
-        <div>
-            {cart.map(p=>(
-                <CardProduct product={p}></CardProduct>
-            ))}
+        <div className='flex justify-center mt-30 '>
+          <h1 className='text-6xl text-gray-200  font-serif'>No Data</h1>
         </div>
 
     </div>
